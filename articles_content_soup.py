f1651c7936638835baab25f93404f2833656864e
EGrandgi <45683057+EGrandgi@users.noreply.github.com>
2019-02-14 22:57:02 +0100

    Update and rename scraping/beautifulsoup_test.py to articles_content_soup.py

--- articles_content_soup.py
+++ articles_content_soup.py
@@ -33,7 +33,7 @@ def create_soup(url):
 def get_articles_urls():
     urls_list = []
 
-    for i in range(1, 40):
+    for i in range(1, 5): #40 pages
         url_search = 'https://www.lemonde.fr/recherche/?keywords=coupe+du+monde+2022+qatar&page_num=' + str(i) +'&operator=and&exclude_keywords=&qt=recherche_texte_titre&author=&period=since_1944&start_day=01&start_month=01&start_year=1944&end_day=29&end_month=01&end_year=2019&sort=desc'
         soup = create_soup(url_search)
     
@@ -46,6 +46,52 @@ def get_articles_urls():
     
 urls_list = get_articles_urls()
 
+
+# =============================================================================
+#                     Get 1 article's content - LE MONDE
+# =============================================================================
+
+def get_1_article_content(url):
+    
+    url_ = url
+    soup = create_soup(url)
+    source = "Le Monde"
+    
+    #title
+    title = soup.find('title').string
+    
+    #subtitle
+    for main in soup.find_all('main'):
+        for p in main.find_all('p'):
+            if p.get("class") != ['article__status']:
+                if p.get("class") == ['article__desc']:
+                    subtitle = p
+                    
+    #content
+    content = ""
+    for body in soup.find_all('body'):
+        if body.get("id") == 'js-body':
+            for article in body.find_all('article'):
+                if article.get("class") == ['article article--single article--iso']:
+                    article.string = ""
+            content += article.get_text() + " "
+    
+    #MANQUE: date, author, theme
+    return(source, url_, title, subtitle, content)
+        
+
+#test with 1 article
+url = urls_list[8]
+source, url_, title, subtitle, content = get_1_article_content(url)
+
+
+#dictionary
+article_content = {"source": source,
+                   "url": url_,
+                   "title": title,
+                   "subtitle": subtitle,
+                   "content": content}
+
                 
 # =============================================================================
 #                     Get the articles' content - LE MONDE
@@ -54,6 +100,7 @@ urls_list = get_articles_urls()
 def get_articles_content():
     
     for url in urls_list:
+        url_ = url
         soup = create_soup(url)
         source = "Le Monde"
         
@@ -66,21 +113,16 @@ def get_articles_content():
                 if p.get("class") != ['article__status']:
                     if p.get("class") == ['article__desc']:
                         subtitle = p
+                        
+        #content
+        content = ""
+        for body in soup.find_all('body'):
+            if body.get("id") == 'js-body':
+                for article in body.find_all('article'):
+                    if article.get("class") == ['article article--single article--iso']:
+                        article.string = ""
+                content += article.get_text() + " "
                     
-        return(source, url, title, subtitle)
+        return(source, url_, title, subtitle, content)
         
-        
 
-data = get_articles_content()
-
-
-
-
-article_content = { "source": source,
-                    "url": url,
-#                    "author": author,
-#                    "date": date,
-#                    "category": category,
-                    "title": title,
-                    "subtitle": subtitle}
-#                    "content": content }
