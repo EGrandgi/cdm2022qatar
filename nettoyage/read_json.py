3ec76c0e5c285eb2ef1dec1a5b6dc626f6b548b8
EGrandgi <45683057+EGrandgi@users.noreply.github.com>
2019-02-22 11:32:01 +0100

    Create read_json.py

---
+++ nettoyage/read_json.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 16 16:08:52 2019
+
+"""
+
+import json
+
+path = 'C:\\Users\\egran\\data\\'
+filename = '2019-02-20_all_articles_courrierint.json'
+f = open(path + filename, encoding='utf-8', )
+doc = json.load(f)
