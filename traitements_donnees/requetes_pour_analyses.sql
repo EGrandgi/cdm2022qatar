8412c3971a14fb7ba2942ddc7989381f7b55ee1f
EGrandgi <45683057+EGrandgi@users.noreply.github.com>
2019-03-22 20:59:34 +0100

    Update requetes_pour_analyses.sql

--- traitements_donnees/requetes_pour_analyses.sql
+++ traitements_donnees/requetes_pour_analyses.sql
@@ -5,13 +5,13 @@ SELECT COUNT(*) FROM TIME_;
 --- 1046 dates distinctes
 
 SELECT COUNT(*) FROM DATER_DE;
---- 2944 lignes : 1 article associé à 1 date
+--- 2944 lignes : 1 article associÃ© Ã  1 date
 
 SELECT COUNT(*) FROM COUNTRIES;
 --- 197 pays
 
 SELECT COUNT(*) FROM PROVENIR_DE;
---- 2944 lignes : 1 article associé à 1 pays
+--- 2944 lignes : 1 article associÃ© Ã  1 pays
 
 SELECT COUNT(*) FROM PERSONS;
 --- 305 personnes
@@ -28,25 +28,25 @@ SELECT COUNT(*) FROM CONCERNER_PL;
 
 SELECT source_, COUNT(*) FROM ARTICLES GROUP BY source_;
 --- nombre d'articles par source
---- LExpress				1009
---- Le Monde				412
---- lequipe					377
---- football365				262
---- 20 minutes				199
---- RMCSport				188
---- Le Point				158
---- France Football			144
---- Eurosport				141
---- Courrier International	54
+--- LExpress		    		      1009
+--- Le Monde			          	412
+--- lequipe				           	377
+--- football365				        262
+--- 20 minutes			        	199
+--- RMCSport				          188
+--- Le Point				          158
+--- France Football		      	144
+--- Eurosport				          141
+--- Courrier International  	54
 
 SELECT id_pers, COUNT(*) FROM CONCERNER_PE GROUP BY id_pers;
---- identifiants des personnes concernées
---- 148     				717
+--- identifiants des personnes concernÃ©es
+--- 148     		      717
 --- 204         			488
---- 139     				202
+--- 139     				  202
 --- 55     	    			195
---- 118     				188
---- 199      				166
+--- 118     				  188
+--- 199      				  166
 
 SELECT id_pers, pers FROM PERSONS WHERE id_pers = '148';    --- joseph_blatter
 SELECT id_pers, pers FROM PERSONS WHERE id_pers = '204';    --- michel_platini
@@ -56,7 +56,7 @@ SELECT id_pers, pers FROM PERSONS WHERE id_pers = '118';    --- gianni_infantino
 SELECT id_pers, pers FROM PERSONS WHERE id_pers = '199';    --- michael_garcia
 
 SELECT SUBSTR(date_, 7) AS Y, COUNT(*) FROM DATER_DE GROUP BY SUBSTR(date_, 7);
---- nb d'articles par année
+--- nb d'articles par annÃ©e
 --- 2010        87
 --- 2011        145
 --- 2012        53
